2c83341adf8743f978be7bd9e9c2b85dac5a22d0
JinWoo_Lee <53039583+dlwlsdn201@users.noreply.github.com>
2024-08-25 16:36:31 +0900

    :wrench: rules:"no-duplicate-imports" 오타 수정

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,7 @@ export default tseslint.config(
       'no-await-in-loop': 'error', // loop 반복문 내에서 await 키워드 사용을 혀용하지 않음. (See: https://eslint.org/docs/latest/rules/no-await-in-loop)
       'no-const-assign': 'error', // const 변수에 값 재할당을 허용하지 않음.
       'no-duplicate-case': 'error', // 같은 스코프에 존재하는 switch-case 구문들 중에서 중복되는 case 구문을 허용하지 않음. (See: https://eslint.org/docs/latest/rules/no-duplicate-case)
-      'no-duplicate-import': 'warn', // 같은 모듈로부터 import 하는 구문이 2개 이상 중복 작성되는 것을 경고함. (See: https://eslint.org/docs/latest/rules/no-duplicate-import)
+      'no-duplicate-imports': 'warn', // 같은 모듈로부터 import 하는 구문이 2개 이상 중복 작성되는 것을 경고함. (See: https://eslint.org/docs/latest/rules/no-duplicate-import)
       'no-unsafe-optional-chaining': 'error', // 안전하지 않은 옵셔널 체이닝 구문을 허용하지 않음. (See: https://eslint.org/docs/latest/rules/no-unsafe-optional-chaining)
       camelcase: ['error', { ignoreDestructing: true, ignoreImports: true }], // 카멜 케이스
       'default-case': 'warn', // case 구문에서 default 항목이 있는지에 대한 여부
